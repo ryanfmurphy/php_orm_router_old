84ffef252f52b08327a2fe55c9bfe4b516487920
ryanfmurphy <ryanfmurphy@gmail.com>
2016-02-16 05:38:17 -0600

    Model::updateExisting() functionality
    factoring / re-org throughout model
    also converted tabs to spaces

--- classes/Model.php
+++ classes/Model.php
@@ -2,82 +2,132 @@
 
 class Model {
 
-	function __construct($vars=array()) {
+    function __construct($vars=array()) {
         $this->updateFields($vars);
-	}
-
-    public function updateFields($vars) {
-		foreach ($vars as $key => $val) {
-			$this->{$key} = $val;
-		}
     }
 
-	public static function create($vars) {
-		if (!count($vars)) trigger_error("Model::create needs at least one key-value pair", E_USER_ERROR);
 
-		$db = Db::conn();
+    # create and populate object, save to db
+    public static function create($vars) {
+        $ClassName = get_called_class();
+        $obj = new $ClassName($vars);
+        return $obj->insertAsNew();
+    }
 
-		# populate object
-		$ClassName = get_called_class();
-		$table_name = ClassName2table_name($ClassName);
-		$obj = new $ClassName($vars);
+    public static function update($vars) {
+        $ClassName = get_called_class();
+        $obj = new $ClassName($vars);
+        return $obj->updateExisting();
+    }
 
-		list($varNameList, $varValList) = Db::sqlFieldsAndValsFromArray(get_object_vars($obj));
-
-		$sql = "
-			insert into $table_name ($varNameList)
-			values ($varValList);
-		";
-		$result = mysqli_query($db, $sql);
-
-		if ($result) {
-            $obj->setId(mysqli_insert_id($db)); #todo fill in default fields too?
-			return $obj;
-		}
-		else {
-            Db::error("Model::create could not create object.", $sql);
-		}
-	}
-
-	public static function get($vars=array()) {
+    public static function get($vars=array()) {
         $idField = self::getIdFieldName();
 
-        { # syntactic sugar
-            /*if (is_string($vars)) {
-                $vars = array( 'where' => $vars );
-            }*/
+        if (is_string($vars)) {
+            $sql = $vars;
+        }
+        else {
+            # syntactic sugar: just pass num to fetch by ID
             if (is_int($vars)) {
                 $vars = array( $idField => $vars );
             }
+
+            $sql = self::buildSelectSql($vars);
         }
 
-        { # build sql
-            $table_name = self::table_name();
-            $sql = "
-                select * from $table_name
-            ";
+        return Model::query_fetch($sql);
+    }
 
-            # add where clauses
-            $whereOrAnd = 'where';
-            /* if (isset($vars['where'])) {
-                $sql .= "\n$whereOrAnd $vars[where]";
-                $whereOrAnd = 'and';
-            } */
-            foreach ($vars as $key => $val) {
-                $val = Db::sqlLiteral($val);
-                $sql .= "\n$whereOrAnd $key = $val";
-                $whereOrAnd = 'and';
-            }
-
-            $sql .= ";";
+    public function updateFields($vars) {
+        foreach ($vars as $key => $val) {
+            $this->{$key} = $val;
         }
-
-		return Model::query_fetch($sql);
-	}
-
+    }
 
 
     public function insertAsNew() {
+        $table_name = self::table_name();
+
+        $objVars = get_object_vars($this);
+        if (!count($objVars)) { #todo is this ultimately necessary?
+            trigger_error("Model::create needs at least one key-value pair", E_USER_ERROR);
+        }
+        list($varNameList, $varValList) = Db::sqlFieldsAndValsFromArray($objVars);
+
+        $sql = "
+            insert into $table_name ($varNameList)
+            values ($varValList);
+        ";
+
+        $db = Db::conn();
+        $result = mysqli_query($db, $sql);
+
+        if ($result) {
+            $this->setId(mysqli_insert_id($db)); #todo fill in default fields too?
+            return $this;
+        }
+        else {
+            Db::error("Model::create could not create object.", $sql);
+        }
+
+    }
+
+    public static function buildSelectSql($vars) {
+        $table_name = self::table_name();
+        $sql = "
+            select * from $table_name
+        ";
+
+        # add where clauses
+        $whereOrAnd = 'where';
+        foreach ($vars as $key => $val) {
+            $val = Db::sqlLiteral($val);
+            $sql .= "\n$whereOrAnd $key = $val";
+            $whereOrAnd = 'and';
+        }
+
+        $sql .= ";";
+        return $sql;
+    }
+
+
+    # save changes of existing obj/row to db
+    public function updateExisting() {
+        $table_name = self::table_name();
+
+        $objVars = get_object_vars($this);
+        list($varNameList, $varValList) = Db::sqlFieldsAndValsFromArray($objVars);
+
+        { # build sql
+            $sql = "
+                update $table_name set
+            ";
+
+            $comma = false;
+            foreach ($objVars as $key => $val) {
+                if ($comma) $sql .= ",";
+                $val = Db::sqlLiteral($val);
+                $sql .= "\n$key = $val";
+                $comma = true;
+            }
+            $idField = self::getIdFieldName();
+            $id = $this->getId();
+            $sql .= "
+                where $idField = $id
+            ";
+            $sql .= ';';
+        }
+
+        $db = Db::conn();
+        $result = mysqli_query($db, $sql);
+
+        if ($result) {
+            return $this;
+        }
+        else {
+            Db::error("Model::create could not create object.", $sql);
+        }
+
     }
 
 
@@ -87,14 +137,13 @@ class Model {
         $this->{$idField} = $val;
     }
 
+    private function getId() {
+        $idField = self::getIdFieldName();
+        return $this->{$idField};
+    }
+
     private static function getIdFieldName() {
         return self::table_name() . '_id';
-
-        /* #todo still need get_called_class??
-        $ClassName = get_called_class();
-        $table_name = ClassName2table_name($ClassName);
-        return $table_name.'_id';
-        */
     }
 
     private static function table_name() {
@@ -102,21 +151,21 @@ class Model {
         return ClassName2table_name($ClassName);
     }
 
-	private static function mysqli_fetch_all($result) {
+    private static function mysqli_fetch_all($result) {
         $ClassName = get_called_class();
-		$rows = array();
-		while ($row = mysqli_fetch_object($result, $ClassName)) {
-			$rows[] = $row;
-		}
-		return $rows;
-	}
+        $rows = array();
+        while ($row = mysqli_fetch_object($result, $ClassName)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
 
-	private static function query_fetch($sql) {
-		$db = Db::conn();
+    private static function query_fetch($sql) {
+        $db = Db::conn();
         $result = mysqli_query($db, $sql);
         $rows = Model::mysqli_fetch_all($result);
         return $rows;
-	}
+    }
 
 }
 
