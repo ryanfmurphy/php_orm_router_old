b7da2d1782099e75d988d59bcd6f990bee415601
ryanfmurphy <ryanfmurphy@gmail.com>
2016-02-15 19:47:07 -0600

    string_case.php util functions

---
+++ util/string_case.php
@@ -0,0 +1,26 @@
+<?php
+
+function CapCase2camelCase($strn) { return lcfirst($strn); }
+function camelCase2CapCase($strn) { return ucfirst($strn); }
+
+function snake_case2CapCase($strn) {
+    $words = str_replace('_', ' ', $strn);
+    return str_replace(' ', '', ucwords($words));
+}
+
+function snake_case2camelCase($strn) {
+    return CapCase2camelCase(
+        snake_case2CapCase($strn)
+    );
+}
+
+function camelCase2snake_case($strn) {
+    return strtolower(
+        preg_replace('/[A-Z]/', '_\0', $strn)
+    );
+}
+
+function CapCase2snake_case($strn) {
+    return camelCase2snake_case(CapCase2camelCase($strn));
+}
+
