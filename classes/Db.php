8aa17dcdcc12da5a3e74d086f22ecff0068fcd1c
ryanfmurphy <ryanfmurphy@gmail.com>
2016-02-16 05:03:05 -0600

    pulled Db class into its own file
    also, re-org on util/includes

---
+++ classes/Db.php
@@ -0,0 +1,32 @@
+<?php
+
+class Db {
+
+    public static function connectToDb() {
+        $db = $GLOBALS['db'] = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        return $db;
+    }
+
+    # connection to db
+    public static function conn() {
+        $db = ( isset($GLOBALS['db'])
+                   ? $GLOBALS['db']
+                   : Db::connectToDb() );
+        if (!$db) {
+            trigger_error('problem connecting to database', E_USER_ERROR);
+        }
+        else {
+            return $db;
+        }
+    }
+
+    public static function error($msg, $sql) {
+        $db = Db::conn();
+        trigger_error($msg
+                     ." SQL error = ".mysqli_error($db)
+                     ." for query '$sql'"
+        , E_USER_ERROR);
+    }
+
+}
+
--- util/includes.php
+++ util/includes.php
@@ -1,16 +1,20 @@
 <?php
 include('or-config.php');
 
-$ControllerClass = null;
-
+# util
 foreach (glob('util/*.php') as $util_file) {
     require_once($util_file);
 }
 
-foreach (glob('classes/*.php') as $class_file) {
-    require_once($class_file);
-}
 
+# classes
+require_once('classes/Db.php');
+require_once('classes/Model.php');
+require_once('classes/Controller.php');
+
+
+# controllers
+$ControllerClass = null;
 foreach (glob('controllers/*.php') as $controller_file) {
     if (!$ControllerClass) {
         #$controller_name = basename($controller_file, '.php');
@@ -23,6 +27,8 @@ if (!$ControllerClass) {
     $ControllerClass = 'Controller';
 }
 
+
+# models
 foreach (glob('models/*.php') as $model_file) {
     require_once($model_file);
 }
