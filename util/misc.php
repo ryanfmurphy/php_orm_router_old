0b72cc90b03f3e4b0d5a9c003e401c37e6594f46
Ryan <ryan@dev.kasita.com>
2016-02-22 14:52:26 -0500

    util/misc: curl_get, curl_post, ...

--- util/misc.php
+++ util/misc.php
@@ -8,16 +8,51 @@ function httpMethod() {
     return $_SERVER['REQUEST_METHOD'];
 }
 
+function curl_get($url, $vars=NULL, $username=NULL, $password=NULL) {
+    return do_curl($url, $vars, null, $username, $password);
+}
+
 function curl_post($url, $vars, $username=NULL, $password=NULL) {
+    return do_curl($url, null, $vars, $username, $password);
+}
+
+function curl_put($url, $vars, $username=NULL, $password=NULL) {
+    return do_curl($url, null, $vars, $username, $password, true);
+}
+
+function do_curl($url, $get_vars=null, $post_vars=NULL, $username=NULL, $password=NULL, $do_PUT_request=false) {
     $ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($vars));
-    if ($username) {
+    { # set the options
+        # get
+        if (is_array($get_vars)) {
+            $query_string = http_build_query($get_vars);
+            if ($query_string) {
+                $url .= "?$query_string";
+            }
+        }
+
+        # url
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        # post
+        if (is_array($post_vars)) {
+            if ($do_PUT_request) {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            }
+            else {
+                curl_setopt($ch, CURLOPT_POST, 1);
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_vars));
+        }
+
+        # auth
+        if ($username) {
+            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($ch, CURLOPT_USERPWD, "$username:$password"); 
+        }
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password"); 
     }
 
     # collect the $result and close the curl handle
@@ -26,9 +61,20 @@ function curl_post($url, $vars, $username=NULL, $password=NULL) {
     return $result;
 }
 
+
+
 function debug_r($arr) {
     echo '<pre>';
     print_r($arr);
     echo '</pre>';
+}
+
+
+function dollars2cents($numDollars) {
+    return $numDollars * 100;
+}
+
+function cents2dollars($numDollars) {
+    return round($numDollars / 100, 2);
 }
 
